a9f544235c76188c1a24754518786c0aba58343a
Khushal Sehrawat <133148875+khushalsehrawat@users.noreply.github.com>
2025-12-04 15:27:30 +0530

    Update LogRepository.java

--- Backend/src/main/java/com/ai/ThreatDetection/repository/LogRepository.java
+++ Backend/src/main/java/com/ai/ThreatDetection/repository/LogRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LogRepository extends JpaRepository<LogEntry, Long> {
 
-
+  List<LogEntry> findByUser(User user);
 
 }
