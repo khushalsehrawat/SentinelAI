a29f2d26284054db0c59f7f79c1fcc5309ebd368
Khushal Sehrawat <133148875+khushalsehrawat@users.noreply.github.com>
2025-12-04 16:45:48 +0530

    Update LogRepository.java

--- Backend/src/main/java/com/ai/ThreatDetection/repository/LogRepository.java
+++ Backend/src/main/java/com/ai/ThreatDetection/repository/LogRepository.java
@@ -1,10 +1,16 @@
 package com.ai.ThreatDetection.repository;
 
 import com.ai.ThreatDetection.entity.LogEntry;
+import com.ai.ThreatDetection.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface LogRepository extends JpaRepository<LogEntry, Long> {
 
-  List<LogEntry> findByUser(User user);
+
+    List<LogEntry> findByUser(User user);
+
+
 
 }
